51a9120cc43f18ba495b5d9b3b3ea06fb75f8eec
Thalis Elias <thalliselias@hotmail.com>
2024-10-11 09:22:03 -0300

    inserindo JS - perguntas frequentes toggle de ativo

--- js/script.js
+++ js/script.js
@@ -23,3 +23,26 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+///////////////////////////////////////////////
+
+//Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+
+  if (resposta.classList.contains("ativa")) {
+    pergunta.setAttribute("aria-expanded", "true");
+  }
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
